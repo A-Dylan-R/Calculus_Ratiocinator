49e39644bdcedd1cd44079c2308b2e10bc80cf66
Aina Dylan Rasamoelina <hei.dylan.3@gmail.com>
2024-06-07 16:14:27 +0300

    feat: affirmation

---
+++ src/Affirmation.java
@@ -0,0 +1,22 @@
+public class Affirmation {
+    private final String texte;
+    private final Boolean valeur; // vrai, faux, ou null pour indécis
+
+    public Affirmation(String texte, Boolean valeur) {
+        this.texte = texte;
+        this.valeur = valeur;
+    }
+
+    public Boolean évaluer() {
+        return this.valeur;
+    }
+
+    public String getTexte() {
+        return texte;
+    }
+
+    public Boolean getValeur() {
+        return valeur;
+    }
+
+}
