524576caea363a629830c2ee6a522c1bcc1dd194
Aina Dylan Rasamoelina <hei.dylan.3@gmail.com>
2024-06-07 16:59:12 +0300

    chore: added one test

--- src/Main.java
+++ src/Main.java
@@ -15,10 +15,12 @@ public class Main {
         Affirmation résultat2 = calculateur.évaluerDonc(verite_1, mensonge_1);
         Affirmation résultat3 = calculateur.évaluerDonc(mensonge_1, affirmation_1);
         Affirmation résultat4 = calculateur.évaluerDonc(calculateur.évaluerOu(verite_1, affirmation_1), mensonge_1);
+        Affirmation résultat5 = calculateur.évaluerOu(mensonge_1, affirmation_1);
 
         System.out.println(résultat1.getTexte() + " : " + (résultat1.getValeur() != null ? résultat1.getValeur() : "jenesaispas"));
         System.out.println(résultat2.getTexte() + " : " + (résultat2.getValeur() != null ? résultat2.getValeur() : "jenesaispas"));
         System.out.println(résultat3.getTexte() + " : " + (résultat3.getValeur() != null ? résultat3.getValeur() : "jenesaispas"));
         System.out.println(résultat4.getTexte() + " : " + (résultat4.getValeur() != null ? résultat4.getValeur() : "jenesaispas"));
+        System.out.println(résultat5.getTexte() + " : " + (résultat5.getValeur() != null ? résultat5.getValeur() : "jenesaispas"));
     }
 }
